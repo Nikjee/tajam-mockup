7c088172d2e723d85caa68dbf0cf95647fe4e367
Nikjee <boss.bold@mail.ru>
2021-11-25 03:47:54 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -4,3 +4,17 @@ $(document).ready(function () {
     arrows: false,
   })
 })
+
+function ibg() {
+  let ibg = document.querySelectorAll('._ibg')
+  for (let i = 0; i < ibg.length; i++) {
+    if (
+      ibg[i].querySelector('img') &&
+      ibg[i].querySelector('img').getAttribute('src') != null
+    ) {
+      ibg[i].style.backgroundImage =
+        'url(' + ibg[i].querySelector('img').getAttribute('src') + ')'
+    }
+  }
+}
+ibg()
